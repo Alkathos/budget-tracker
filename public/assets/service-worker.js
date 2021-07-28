8c1260d83ff98f48e5ae8331a34fb7d7fa4bf669
Fernando Zaldivar <76067527+Alkathos@users.noreply.github.com>
2021-07-28 06:26:27 -0400

    Added code to db.js and service-worker.js

--- public/assets/service-worker.js
+++ public/assets/service-worker.js
@@ -15,22 +15,18 @@ const FILES_TO_CACHE = [
 ];
 
 // install
-self.addEventListener("install", function (evt) {
-    // pre cache image data
-    evt.waitUntil(
-      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
-      );
-      
-    // pre cache all static assets
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-    );
-  
-    // tell the browser to activate this service worker immediately once it
-    // has finished installing
-    self.skipWaiting();
-  });
+self.addEventListener("install", function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 
+  self.skipWaiting();
+});
+
+// activate
 self.addEventListener("activate", function(evt) {
   evt.waitUntil(
     caches.keys().then(keyList => {
@@ -49,39 +45,32 @@ self.addEventListener("activate", function(evt) {
 });
 
 // fetch
-self.addEventListener("fetch", function(evt) {
-  // cache successful requests to the API
-  if (evt.request.url.includes("/api/")) {
-    console.log("[Service Worker] Fetch (data)", evt.request.url);
+self.addEventListener("fetch", evt => {
+    if(evt.request.url.includes('/api/')) {
+        console.log('[Service Worker] Fetch(data)', evt.request.url);
+    
+evt.respondWith(
+                caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                .then(response => {
+                    if (response.status === 200){
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(evt.request);
+                });
+            })
+            );
+            return;
+        }
 
-    evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(evt.request)
-          .then(response => {
-            // If the response was good, clone it and store it in the cache.
-            if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
-            }
-
-            return response;
-          })
-          .catch(err => {
-            // Network request failed, try to get it from the cache.
-            return cache.match(evt.request);
-          });
-      }).catch(err => console.log(err))
-    );
-
-    return;
-  }
-
-  // if the request is not for the API, serve static assets using "offline-first" approach.
-  // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
-  evt.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
-        return cache.match(evt.request).then(response => {
-            return response || fetch(evt.request);
-        });
+evt.respondWith(
+    caches.open(CACHE_NAME).then( cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
     })
   );
 });
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-require('dotenv').config();
 
 
 const PORT = process.env.PORT || 3000;
@@ -17,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
