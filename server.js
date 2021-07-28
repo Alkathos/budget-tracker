06af496e6c1f4d9bf574a40d707be4d3eb96d103
Fernando Zaldivar <76067527+Alkathos@users.noreply.github.com>
2021-07-28 07:28:22 -0400

    Adjusted server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -18,8 +17,6 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
 });
 
 // routes
